2cf804e832d19f4bb9df58f02ef68db979261adf
Bruno Pianca <bruno.pianca@alstratech.com>
2022-08-29 20:27:12 -0300

    init: Backend application

--- 1-arrow-functions.js
+++
@@ -1,9 +0,0 @@
-// Função simples
-function simples(parametro) {
-    return console.log('Retorno da função simples!', parametro);
-}
-simples('Teste');
-
-// Arrow function
-const funcaoArrow = (parametro) => { return console.log('Retorno do arrow function!', parametro); }
-funcaoArrow('Teste');
