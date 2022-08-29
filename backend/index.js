a3039cd0ca267f27a41523db48023c66c0c0d210
Bruno Pianca <bruno.pianca@alstratech.com>
2022-08-29 20:58:06 -0300

    init: Django backend application

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => { return res.send('Rota principal'); });
-
-app.listen(3000, () => console.log('Servidor rodando'));
